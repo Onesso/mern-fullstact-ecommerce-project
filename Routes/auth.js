ed10e86b28320a5466974618bc620ab2ee395ed6
Frank Onesso <odhiambofrank1965@gmail.com>
2023-10-25 03:37:07 +0300

    server side route protection

--- Routes/auth.js
+++ Routes/auth.js
@@ -16,6 +16,13 @@ router.post("/register", register);
 //login route with the login function
 router.post("/login", login);
 
+//this is going to be used in the client side to see if the  as a verification to a particular route
+//the call back function can be put in a controller function but since it is simple can be wrriten here
+
+router.get("/auth-check",requireSignin, (req, res)=>{
+    res.json({ok: true})
+})
+
 //testing
 router.get("/secret", requireSignin, isAdmin, secret); // now this means that this route is only available for loged in users this is by adding the middleware
 //"requireSignin"
